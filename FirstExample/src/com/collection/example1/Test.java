18297910efb35d575777014dd8c0b436bbd48d4a
Sushant071001 <ssp110266@gmail.com>
2022-11-15 18:25:04 +0530

    added Test.javfa file into the project

---
+++ FirstExample/src/com/collection/example1/Test.java
@@ -0,0 +1,12 @@
+package com.collection.example1;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Sushant is best student & very bad teacher");
+		
+	}
+
+}
