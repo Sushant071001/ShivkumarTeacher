28ca779afefab3ae9dadac85a87933bd2868f38d
Sushant071001 <ssp110266@gmail.com>
2022-11-15 21:08:37 +0530

    test.java changed

--- FirstExample/src/com/collection/example1/Test.java
+++ FirstExample/src/com/collection/example1/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
 		System.out.println("Sushant is best student & very bad teacher");
 
-
+		System.out.println("ssss");
 		System.out.println("siddhant is best mentor");
 		
 		System.out.println("Diksha is best English teacher");
